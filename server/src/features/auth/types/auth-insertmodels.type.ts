e6d9044c725e639b1974029b11aab341f40d56db
steins0668 <archelausagaton@gmail.com>
2025-09-25 22:24:07 +0800

    feat: add `NewRole` insert model

--- server/src/features/auth/types/auth-insertmodels.type.ts
+++ server/src/features/auth/types/auth-insertmodels.type.ts
@@ -1,5 +1,6 @@
 import * as schema from "../../../models";
 
+export type NewRole = typeof schema.Role.$inferInsert;
 export type NewSessionToken = typeof schema.SessionToken.$inferInsert;
 export type NewUser = typeof schema.User.$inferInsert;
 export type NewUserSession = typeof schema.UserSession.$inferInsert;
