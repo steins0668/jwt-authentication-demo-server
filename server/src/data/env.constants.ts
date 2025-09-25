0999474bd9f4d9507fe15e72d8f7702ff6f9448c
steins0668 <archelausagaton@gmail.com>
2025-09-25 16:49:14 +0800

    refactor: rename DB_FILENAME to DB_URL for better uniformity

--- server/src/data/env.constants.ts
+++ server/src/data/env.constants.ts
@@ -1,6 +1,6 @@
-export function getDb(): string {
-  const dbName: string | undefined = process.env.DB_FILENAME;
-  if (!dbName) throw new Error("DB filename not configured.");
+export function getDbUrl(): string {
+  const dbName: string | undefined = process.env.DB_URL;
+  if (!dbName) throw new Error("DB URL not configured.");
 
   return dbName;
 }
