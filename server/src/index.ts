052d315c057e3b39e3670218972cf96120f39976
steins0668 <archelausagaton@gmail.com>
2025-09-25 16:11:07 +0800

    feat: add index.ts file

---
+++ server/src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import cors, { type CorsOptions } from "cors";
+
+const app = express();
+
+const whitelist = ["http://localhost:[port]"];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || whitelist.includes(origin)) callback(null, true);
+    else callback(new Error("Not allowed by CORS."));
+  },
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); // json parsing
+
+const port = 2620; //  can be anything
+app.listen(port, "0.0.0.0", () => {
+  //  the 0.0.0.0 is for hosting on your local ip
+  console.log("Server is now running at port: " + port);
+});
