7ff1eeec9cac52486cb4887366f6e864d1f3951e
steins0668 <archelausagaton@gmail.com>
2025-09-25 18:43:26 +0800

    fix: fix schema directory in drizzle config

--- server/config/drizzle.config.ts
+++ server/config/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { ENV } from "../src/data";
 
 export default defineConfig({
-  schema: "./build/src/models/index.js",
+  schema: "./build/models/index.js",
   out: "./migrations",
   // dialect: "turso",
   dialect: "sqlite",
