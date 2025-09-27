fb33010ed46a6344f6a4c2655755fddbdf88785b
steins0668 <archelausagaton@gmail.com>
2025-09-28 00:47:36 +0800

    feat: implement logging and result object based logic in `handleRegister` controller.

--- server/src/features/auth/controllers/handle-register.ts
+++ server/src/features/auth/controllers/handle-register.ts
@@ -5,25 +5,47 @@ export async function handleRegister(
   req: Request<{}, {}, RegisterSchema>,
   res: Response
 ) {
-  //  todo: validate if user exists
   const { body: user, requestLogger, userDataService } = req;
 
-  const foundUser = await userDataService.tryGetUser({
+  const queryResult = await userDataService.tryGetUser({
     type: "user",
     user,
   });
 
-  if (foundUser) {
+  if (queryResult.success && queryResult.result) {
+    //  query completed and there is a result
     const msg = "User already exists.";
+
     requestLogger.log("debug", msg);
     res.status(409).json({ msg });
+    return;
+  }
+  if (!queryResult.success) {
+    //  query interrupted
+    const { message: logMsg } = queryResult.error;
+    requestLogger.log("error", logMsg, queryResult.error);
+
+    const resMsg = "Database error. Please try again later.";
+    res.status(500).json({ msg: resMsg });
+    return;
   }
 
   // inserting user
-  const inserted = await userDataService.tryAddUser(user);
+  requestLogger.log("debug", "Inserting new user...");
+  const insertResult = await userDataService.tryAddUser(user);
 
-  // todo: add logging
-  return inserted
-    ? res.status(201).json({ msg: "User registration success." })
-    : res.status(409).json({ msg: "User registration failed." });
+  if (insertResult.success) {
+    const msg = "User registration success.";
+
+    requestLogger.log("debug", msg);
+    res.status(201).json({ msg });
+  } else {
+    const { message: logMsg } = insertResult.error;
+    requestLogger.log("error", logMsg, insertResult.error);
+
+    const resMsg = "Database error. Please try again later.";
+    res.status(500).json({ ms: resMsg });
+  }
+
+  return;
 }
