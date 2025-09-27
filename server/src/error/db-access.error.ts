4740a337ea42b62d83404ed8ccafafb5bf90d310
steins0668 <archelausagaton@gmail.com>
2025-09-27 23:32:36 +0800

    feat: add default error types including `BaseError`, `DbAccess` Error, and `EnvError`

---
+++ server/src/error/db-access.error.ts
@@ -0,0 +1,10 @@
+import { BaseError } from "./base.error";
+
+export namespace DbAccess {
+  export type ErrorName =
+    | "DB_ACCESS_EXCEEDED_MAX_FETCH_RETRIES_ERROR"
+    | "DB_ACCESS_EMPTY_TABLE_ERROR"
+    | "DB_ACCESS_INSERT_ERROR"
+    | "DB_ACCESS_QUERY_ERROR";
+  export class ErrorClass extends BaseError<ErrorName> {}
+}
