346958a5334b37da7e0429c1f58445f1df4f75cc
steins0668 <archelausagaton@gmail.com>
2025-09-29 02:15:19 +0800

    feat: add error class for sign-in errors

--- server/src/features/auth/error/index.ts
+++ server/src/features/auth/error/index.ts
@@ -1,1 +1,2 @@
 export * from "./register.error";
+export * from "./sign-in.error";
---
+++ server/src/features/auth/error/sign-in.error.ts
@@ -0,0 +1,22 @@
+import { BaseError } from "../../../error";
+import { isError } from "../../../utils";
+
+export namespace SignIn {
+  export type ErrorName = "SIGN_IN_INVALID_CREDENTIALS_ERROR";
+
+  export class ErrorClass extends BaseError<ErrorName> {}
+
+  export function normalizeError<E extends ErrorName>({
+    name,
+    message,
+    err,
+  }: {
+    name: E;
+    message: string;
+    err: unknown;
+  }) {
+    if (isError(ErrorClass, err)) return err;
+
+    return new ErrorClass({ name, message, cause: err });
+  }
+}
