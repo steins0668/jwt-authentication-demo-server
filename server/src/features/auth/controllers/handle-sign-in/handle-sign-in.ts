f227e0f64ece49b90d61e4bc262327bbf3a1ec5a
steins0668 <archelausagaton@gmail.com>
2025-09-29 02:30:45 +0800

    feat: add user verification flow in `handleSignIn` controller.

--- server/src/features/auth/controllers/handle-sign-in/handle-sign-in.ts
+++ server/src/features/auth/controllers/handle-sign-in/handle-sign-in.ts
@@ -1,5 +1,8 @@
 import type { Request, Response } from "express";
+import { ResultBuilder } from "../../../../utils";
+import * as AuthError from "../../error";
 import { SignInSchema } from "../../schemas";
+import { getVerifiedUser } from "./get-verified-user";
 
 export async function handleSignIn(
   req: Request<{}, {}, SignInSchema>,
@@ -8,4 +11,22 @@ export async function handleSignIn(
   const { body: authDetails, requestLogger: logger, userDataService } = req;
 
   //  validate user credentials
+  const verifiedUser = await getVerifiedUser(req);
+
+  if (!verifiedUser) {
+    //  authentication failed
+    const error = new AuthError.SignIn.ErrorClass({
+      name: "SIGN_IN_INVALID_CREDENTIALS_ERROR",
+      message: "Incorrect email, username, or password. Please try again.",
+    });
+
+    const failResult = ResultBuilder.fail(error);
+
+    const logMsg = `Failed sign-in attempt from user ${authDetails.identifier}.`;
+    logger.log("debug", logMsg);
+    res.status(401).json(failResult);
+    return;
+  }
+
+  const { isPersistentAuth } = authDetails;
 }
