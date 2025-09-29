4fe445a6e790385984e6d17eac52091caaac9510
steins0668 <archelausagaton@gmail.com>
2025-09-30 01:52:36 +0800

    feat: add `SessionTokenRotator` service

---
+++ server/src/features/auth/services/user-session/session-token-rotator.ts
@@ -0,0 +1,129 @@
+import { TxContext } from "../../../../db/createContext";
+import { DbAccess } from "../../../../error";
+import { BaseResult } from "../../../../types";
+import { HashUtil, ResultBuilder } from "../../../../utils";
+import { SessionTokenRepository, UserSessionRepository } from "../repositories";
+
+export class SessionTokenRotator {
+  private readonly _sessionTokenRepository: SessionTokenRepository;
+  private readonly _userSessionRepository: UserSessionRepository;
+
+  constructor(
+    sessionTokenRepository: SessionTokenRepository,
+    userSessionTokenRepository: UserSessionRepository
+  ) {
+    this._sessionTokenRepository = sessionTokenRepository;
+    this._userSessionRepository = userSessionTokenRepository;
+  }
+
+  public async rotate(data: {
+    sessionNumber: string;
+    oldToken: string;
+    newToken: string;
+  }): Promise<
+    | BaseResult.Success<number, "TOKEN_ROTATION">
+    | BaseResult.Fail<DbAccess.ErrorClass>
+  > {
+    const { sessionNumber, oldToken, newToken } = data;
+
+    try {
+      const result = await this._userSessionRepository.execTransaction(
+        async (tx) => {
+          //  updated last used for session
+          const updatedId = await this.updateLastUsed({ tx, sessionNumber });
+
+          //  invalidate old token
+          await this.invalidateToken({ tx, oldToken });
+
+          const newTknHash = HashUtil.byCrypto(newToken);
+
+          await this.ensureTokenUnused({ tx, tknHash: newTknHash });
+
+          //  add new token
+          const newTknId = await this._sessionTokenRepository.insertToken({
+            dbOrTx: tx,
+            sessionToken: {
+              sessionId: updatedId,
+              tokenHash: newTknHash,
+              createdAt: new Date().toISOString(),
+            },
+          });
+
+          if (newTknId === undefined)
+            throw new Error("Failed to store new token.");
+
+          return updatedId;
+        }
+      );
+
+      return ResultBuilder.success(result, "TOKEN_ROTATION");
+    } catch (err) {
+      const error: DbAccess.ErrorClass = {
+        name: "DB_ACCESS_INSERT_ERROR",
+        message: "Failed updating session.",
+        cause: err,
+      };
+
+      return ResultBuilder.fail(error);
+    }
+  }
+  //#region updateSession helpers
+  /**
+   * Update `lastUsed` field for session with the matching hash of the provided
+   * `sessionNumber`.
+   * @param options
+   * @returns
+   */
+  private async updateLastUsed(options: {
+    tx: TxContext;
+    sessionNumber: string;
+  }): Promise<number> {
+    //  updated last used for session
+    const updatedId = await this._userSessionRepository.updateLastUsed({
+      dbOrTx: options.tx,
+      queryBy: "session_hash",
+      sessionHash: HashUtil.byCrypto(options.sessionNumber),
+    });
+
+    if (updatedId === undefined) throw new Error("Failed updating session.");
+
+    return updatedId;
+  }
+
+  /**
+   * Invalidate the provided token string if found in the database by
+   * setting the `isUsed` field to `true`.
+   * @param options
+   */
+  private async invalidateToken(options: {
+    tx: TxContext;
+    oldToken: string;
+  }): Promise<void> {
+    await this._sessionTokenRepository.invalidateTokens({
+      dbOrTx: options.tx,
+      queryBy: "token_hash",
+      tokenHash: HashUtil.byCrypto(options.oldToken),
+    });
+  }
+
+  /**
+   * Get the stored tokens in the database matching the provided hash.
+   * If one of them has the `isUsed` field set to `true`, throw an error.
+   * @param options
+   */
+  private async ensureTokenUnused(options: {
+    tx: TxContext;
+    tknHash: string;
+  }): Promise<void> {
+    const usedTokens = await this._sessionTokenRepository.getTokens({
+      dbOrTx: options.tx,
+      queryBy: "token_hash",
+      tokenHash: options.tknHash,
+    });
+
+    //  todo: add fallback behavior to this (delete/logout all sessions)
+    if (usedTokens.some((token) => token.isUsed))
+      throw new Error("Token already used!!");
+  }
+  //#endregion
+}
