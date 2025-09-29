47b67f86ce6d1f2610a2e26cb378f1a603c91761
steins0668 <archelausagaton@gmail.com>
2025-09-29 17:36:30 +0800

    feat: add singleton instance of the dbContext

--- server/src/db/createContext.ts
+++ server/src/db/createContext.ts
@@ -1,18 +1,11 @@
 import { ExtractTablesWithRelations } from "drizzle-orm";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { BetterSQLite3Database, drizzle } from "drizzle-orm/better-sqlite3";
 import { SQLiteTransaction } from "drizzle-orm/sqlite-core";
 import Database from "better-sqlite3";
 import { ENV } from "../data";
 import * as schema from "../models";
 
-export async function createContext() {
-  const sqlite = new Database(ENV.getDbUrl());
-  sqlite.exec("PRAGMA foreign_keys=ON;");
-
-  return drizzle({ client: sqlite, schema });
-}
-
-export type DbContext = Awaited<ReturnType<typeof createContext>>;
+export type DbContext = BetterSQLite3Database<typeof schema>;
 export type TxContext = SQLiteTransaction<
   "async",
   Database.RunResult,
@@ -25,3 +18,16 @@ export type TxContext = SQLiteTransaction<
 // ) => any
 //   ? T
 //   : never;
+
+let dbContext: DbContext | undefined = undefined;
+
+export async function createContext(): Promise<DbContext> {
+  const sqlite = new Database(ENV.getDbUrl());
+  sqlite.exec("PRAGMA foreign_keys=ON;");
+
+  if (!dbContext) {
+    dbContext = drizzle({ client: sqlite, schema });
+  }
+
+  return dbContext;
+}
