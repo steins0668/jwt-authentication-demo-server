298d6e23437908994f17a1cbd7977896dbe3c379
steins0668 <archelausagaton@gmail.com>
2025-09-30 01:55:51 +0800

    chore: code cleanup

--- server/src/features/auth/services/user-session/user-session.service.ts
+++ server/src/features/auth/services/user-session/user-session.service.ts
@@ -3,7 +3,6 @@ import { createContext, TxContext } from "../../../../db/createContext";
 import { DbAccess } from "../../../../error";
 import { BaseResult } from "../../../../types";
 import { HashUtil, ResultBuilder } from "../../../../utils";
-import { InsertModels } from "../../types";
 import { SessionTokenRepository, UserSessionRepository } from "../repositories";
 
 export async function createUserSessionService() {
@@ -78,11 +77,16 @@ export class UserSessionService {
           throw new Error("Failed creating session.");
 
         //  store first refresh token
-        await this.insertToken({
-          tx,
-          sessionId,
-          tokenHash: HashUtil.byCrypto(refreshToken),
+        const tknId = await this._sessionTokenRepository.insertToken({
+          dbOrTx: tx,
+          sessionToken: {
+            sessionId,
+            tokenHash: HashUtil.byCrypto(refreshToken),
+            createdAt: new Date().toISOString(),
+          },
         });
+
+        if (tknId === undefined) throw new Error("Token creation failed.");
 
         return sessionId;
       });
@@ -93,53 +97,6 @@ export class UserSessionService {
         name: "DB_ACCESS_INSERT_ERROR",
         message:
           "An error occured while creating session. Please try again later.",
-        cause: err,
-      };
-
-      return ResultBuilder.fail(error);
-    }
-  }
-
-  //    todo: add docs
-  public async updateSession(data: {
-    sessionNumber: string;
-    oldToken: string;
-    newToken: string;
-  }): Promise<
-    | BaseResult.Success<number, "DB_UPDATE">
-    | BaseResult.Fail<DbAccess.ErrorClass>
-  > {
-    const { sessionNumber, oldToken, newToken } = data;
-
-    try {
-      const result = await this._userSessionRepository.execTransaction(
-        async (tx) => {
-          //  updated last used for session
-          const updatedId = await this.updateLastUsed({ tx, sessionNumber });
-
-          //  invalidate old token
-          await this.invalidateToken({ tx, oldToken });
-
-          const newTknHash = HashUtil.byCrypto(newToken);
-
-          await this.ensureTokenUnused({ tx, tknHash: newTknHash });
-
-          //  add new token
-          await this.insertToken({
-            tx,
-            sessionId: updatedId,
-            tokenHash: newTknHash,
-          });
-
-          return updatedId;
-        }
-      );
-
-      return ResultBuilder.success(result, "DB_UPDATE");
-    } catch (err) {
-      const error: DbAccess.ErrorClass = {
-        name: "DB_ACCESS_INSERT_ERROR",
-        message: "Failed updating session.",
         cause: err,
       };
 
@@ -240,88 +197,5 @@ export class UserSessionService {
       };
       return ResultBuilder.fail(error);
     }
-  }
-  //#region updateSession helpers
-  /**
-   * Update `lastUsed` field for session with the matching hash of the provided
-   * `sessionNumber`.
-   * @param options
-   * @returns
-   */
-  private async updateLastUsed(options: {
-    tx: TxContext;
-    sessionNumber: string;
-  }): Promise<number> {
-    //  updated last used for session
-    const updatedId = await this._userSessionRepository.updateLastUsed({
-      dbOrTx: options.tx,
-      queryBy: "session_hash",
-      sessionHash: HashUtil.byCrypto(options.sessionNumber),
-    });
-
-    if (updatedId === undefined) throw new Error("Failed updating session.");
-
-    return updatedId;
-  }
-
-  /**
-   * Invalidate the provided token string if found in the database by
-   * setting the `isUsed` field to `true`.
-   * @param options
-   */
-  private async invalidateToken(options: {
-    tx: TxContext;
-    oldToken: string;
-  }): Promise<void> {
-    await this._sessionTokenRepository.invalidateTokens({
-      dbOrTx: options.tx,
-      queryBy: "token_hash",
-      tokenHash: HashUtil.byCrypto(options.oldToken),
-    });
-  }
-
-  /**
-   * Get the stored tokens in the database matching the provided hash.
-   * If one of them has the `isUsed` field set to `true`, throw an error.
-   * @param options
-   */
-  private async ensureTokenUnused(options: {
-    tx: TxContext;
-    tknHash: string;
-  }): Promise<void> {
-    const usedTokens = await this._sessionTokenRepository.getTokens({
-      dbOrTx: options.tx,
-      queryBy: "token_hash",
-      tokenHash: options.tknHash,
-    });
-
-    //  todo: add fallback behavior to this (delete/logout all sessions)
-    if (usedTokens.some((token) => token.isUsed))
-      throw new Error("Token already used!!");
-  }
-  //#endregion
-
-  /**
-   * @description Insert a new token with the provided `sessionId` and `tokenHash`.
-   * Automatically generates a new date ISO string for the `createdAt` field.
-   * @throws `Error` if the the token creation failed.
-   * @param options
-   */
-  private async insertToken(options: {
-    tx: TxContext;
-    sessionId: number;
-    tokenHash: string;
-  }): Promise<number> {
-    const newTknId = await this._sessionTokenRepository.insertToken({
-      dbOrTx: options.tx,
-      sessionToken: {
-        ...options,
-        createdAt: new Date().toISOString(),
-      },
-    });
-
-    if (newTknId === undefined) throw new Error("Failed creating new token.");
-
-    return newTknId;
   }
 }
