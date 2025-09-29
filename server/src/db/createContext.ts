b09a3f7f2cf652399c9aec7ead6a649ca0ac2a62
steins0668 <archelausagaton@gmail.com>
2025-09-29 17:25:10 +0800

    feat: add `TxContext` type

--- server/src/db/createContext.ts
+++ server/src/db/createContext.ts
@@ -1,4 +1,6 @@
+import { ExtractTablesWithRelations } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import { SQLiteTransaction } from "drizzle-orm/sqlite-core";
 import Database from "better-sqlite3";
 import { ENV } from "../data";
 import * as schema from "../models";
@@ -11,3 +13,15 @@ export async function createContext() {
 }
 
 export type DbContext = Awaited<ReturnType<typeof createContext>>;
+export type TxContext = SQLiteTransaction<
+  "async",
+  Database.RunResult,
+  typeof schema,
+  ExtractTablesWithRelations<typeof schema>
+>;
+//  ? better version
+// export type TxContext = Parameters<DbContext["transaction"]>[0] extends (
+//   cb: (tx: infer T) => any
+// ) => any
+//   ? T
+//   : never;
