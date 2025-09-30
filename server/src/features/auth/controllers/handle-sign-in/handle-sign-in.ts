9df14209dfe1a17c1ab610597f22c54a65dab5c8
steins0668 <archelausagaton@gmail.com>
2025-09-30 22:30:05 +0800

    feat: implement user verification logic in `handleSignInController`

--- server/src/features/auth/controllers/handle-sign-in/handle-sign-in.ts
+++ server/src/features/auth/controllers/handle-sign-in/handle-sign-in.ts
@@ -1,8 +1,10 @@
 import type { Request, Response } from "express";
-import { ResultBuilder } from "../../../../utils";
+import { ResultBuilder, StatusCode } from "../../../../utils";
+import { AuthStatusCode } from "../../data";
 import * as AuthError from "../../error";
 import { SignInSchema } from "../../schemas";
 import { verifyUser } from "./verify-user";
+import { getSignInMethod } from "./get-sign-in-method";
 
 export async function handleSignIn(
   req: Request<{}, {}, SignInSchema>,
@@ -10,23 +12,39 @@ export async function handleSignIn(
 ) {
   const { body: authDetails, requestLogger: logger, userDataService } = req;
 
-  //  validate user credentials
-  const verifiedUser = await verifyUser(req);
+  //  validate and verify user credentials
+  const verificationResult = await verifyUser(req);
 
-  if (!verifiedUser) {
+  if (!verificationResult.success) {
     //  authentication failed
-    const error = new AuthError.SignIn.ErrorClass({
-      name: "SIGN_IN_INVALID_CREDENTIALS_ERROR",
-      message: "Incorrect email, username, or password. Please try again.",
+    const { error } = verificationResult;
+    const { name, message } = error;
+
+    const statusCode = StatusCode.fromError({
+      errorName: name,
+      statusCodeMap: AuthStatusCode.SignInError,
     });
+    res.status(statusCode).json({ message });
 
-    const failResult = ResultBuilder.fail(error);
+    const safeId = getSafeId(authDetails.identifier);
+    const logMsg = `Failed sign-in attempt from user ${safeId}.`;
+    logger.log("error", logMsg, error);
 
-    const logMsg = `Failed sign-in attempt from user ${authDetails.identifier}.`;
-    logger.log("debug", logMsg);
-    res.status(401).json(failResult);
     return;
   }
 
   const { isPersistentAuth } = authDetails;
+}
+
+function getSafeId(identifier: string): string {
+  const signInMethod = getSignInMethod(identifier);
+
+  switch (signInMethod) {
+    case "email":
+      return identifier.replace(/^(.{2}).*(@.*)$/, "$1***$2"); //  mask emails
+    case "username":
+      return identifier.slice(0, Math.min(8, identifier.length)) + "***";
+    default:
+      return JSON.stringify(identifier).slice(0, 50);
+  }
 }
