28894b76e077e9a6f9652f53e2bebffca7e4088f
steins0668 <archelausagaton@gmail.com>
2025-10-01 00:50:14 +0800

    feat: add status code map for `Session` error namespace

--- server/src/features/auth/data/auth-status-code.map.ts
+++ server/src/features/auth/data/auth-status-code.map.ts
@@ -1,10 +1,18 @@
 import { Exhaustive } from "../../../types";
-import { SignIn } from "../error";
+import { Session, SignIn } from "../error";
 
 export namespace AuthStatusCode {
   export const SignInError: Exhaustive<SignIn.ErrorName> = {
     SIGN_IN_INVALID_CREDENTIALS_ERROR: 401, //  unauthorized
     SIGN_IN_VERIFICATION_ERROR: 401, //  unauthorized
     SIGN_IN_SYSTEM_ERROR: 500, //  internal server error
+  };
+
+  export const SessionError: Exhaustive<Session.ErrorName> = {
+    SESSION_CLEANUP_ERROR: 500,
+    SESSION_START_ERROR: 500,
+    SESSION_TOKEN_CREATION_ERROR: 500,
+    SESSION_TOKEN_REUSE_ERROR: 403,
+    SESSION_TOKEN_ROTATION_ERROR: 500,
   };
 }
