e46bbaee3852132222ac7242840a4de5b49985cb
steins0668 <archelausagaton@gmail.com>
2025-10-01 23:24:36 +0800

    feat: add new `SignInResult` type namespace

--- server/src/features/auth/types/index.ts
+++ server/src/features/auth/types/index.ts
@@ -3,3 +3,4 @@ export * as Tables from "./auth-tables.type";
 export * from "./auth-token.type";
 export * as ViewModels from "./auth-viewmodels.type";
 export * from "./session-result.type";
+export * from "./sign-in-result.type";
