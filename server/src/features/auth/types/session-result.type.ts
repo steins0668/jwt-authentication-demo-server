d512f36be6cedc3fcc434947431bc3be2fab9242
steins0668 <archelausagaton@gmail.com>
2025-10-02 10:26:42 +0800

    feat: add `SESSION_REFRESH` success source for `SessionResult`

--- server/src/features/auth/types/session-result.type.ts
+++ server/src/features/auth/types/session-result.type.ts
@@ -2,7 +2,11 @@ import { BaseResult } from "../../../types";
 import { Session } from "../error";
 
 export namespace SessionResult {
-  type SuccessSource = "SESSION_START" | "SESSION_END" | "TOKEN_ROTATION";
+  type SuccessSource =
+    | "SESSION_START"
+    | "SESSION_END"
+    | "TOKEN_ROTATION"
+    | "SESSION_REFRESH";
   export type Success<TResult> = BaseResult.Success<TResult, SuccessSource>;
   export type Fail = BaseResult.Fail<Session.ErrorClass>;
 }
