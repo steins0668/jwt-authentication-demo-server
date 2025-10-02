588ff6d0325267f4a436b2464d3db52ae7b64ca0
steins0668 <archelausagaton@gmail.com>
2025-10-02 18:25:20 +0800

    refactor: organized imports and exports in `AuthStatusCode`

--- server/src/features/auth/data/auth-status-code.map.ts
+++ server/src/features/auth/data/auth-status-code.map.ts
@@ -1,14 +1,19 @@
 import { Exhaustive } from "../../../types";
-import { AuthConfig, Session, SignIn } from "../error";
+import * as AuthError from "../error";
 
 export namespace AuthStatusCode {
-  export const SignInError: Exhaustive<SignIn.ErrorName> = {
+  export const AuthConfigError: Exhaustive<AuthError.AuthConfig.ErrorName> = {
+    AUTH_CONFIG_COOKIE_CONFIG_ERROR: 500,
+    AUTH_CONFIG_ENV_TKN_SECRET_ERROR: 500,
+  } as const;
+
+  export const SignInError: Exhaustive<AuthError.SignIn.ErrorName> = {
     SIGN_IN_INVALID_CREDENTIALS_ERROR: 401, //  unauthorized
     SIGN_IN_VERIFICATION_ERROR: 401, //  unauthorized
     SIGN_IN_SYSTEM_ERROR: 500, //  internal server error
   } as const;
 
-  export const SessionError: Exhaustive<Session.ErrorName> = {
+  export const SessionError: Exhaustive<AuthError.Session.ErrorName> = {
     SESSION_CLEANUP_ERROR: 500,
     SESSION_START_ERROR: 500,
     SESSION_TOKEN_CREATION_ERROR: 500,
@@ -17,10 +22,5 @@ export namespace AuthStatusCode {
     SESSION_TOKEN_REUSE_ERROR: 403,
     SESSION_TOKEN_ROTATION_ERROR: 500,
     SESSION_REFRESH_ERROR: 500,
-  } as const;
-
-  export const AuthConfigError: Exhaustive<AuthConfig.ErrorName> = {
-    AUTH_CONFIG_COOKIE_CONFIG_ERROR: 500,
-    AUTH_CONFIG_ENV_TKN_SECRET_ERROR: 500,
   } as const;
 }
